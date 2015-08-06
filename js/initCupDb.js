d7bdaab42c889d42b4ac117293ba7a2378178a26
Jeffrey Qua <jeffrey.qua@gmail.com>
2015-08-06 15:47:11 -0700

    Firebase hooks

---
+++ js/initCupDb.js
@@ -0,0 +1,88 @@
+// initialize the database
+function initDbTotals() {
+  // check if the current date is accurate
+  var startDate = new Date("2015-08-05");
+  var currentDate = new Date();
+  var timeDiff = Math.abs(currentDate.getTime() - startDate.getTime());
+  var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)) - 1; 
+
+  var currentDateYmd = moment().format("YYYY-MM-DD");
+
+  var fbRef = new Firebase('https://papercup22.firebaseio.com/');
+  fbRef.on('value', function(snapshot) {
+    var fbObj = snapshot.val();
+    var cupData = fbObj.paper_cup_count;
+    var cupTotals = fbObj.paper_cup_totals;
+    for (var i = diffDays; i > 0; i--) {
+      var iDate = new Date();
+      iDate.setDate(iDate.getDate() - i);
+      var iDateYmd = moment(iDate).format("YYYY-MM-DD");
+      // console.log(iDateYmd);
+
+      if (!cupTotals || !cupTotals[iDateYmd]) {
+        var iDateTotal = 0;
+        if (cupData[iDateYmd]) {
+          iDateTotal = calculateFirebaseObjDayTotal(cupData[iDateYmd]);
+
+          // Update the total
+          var iDateRef = new Firebase('https://papercup22.firebaseio.com/paper_cup_totals/'+iDateYmd);
+          iDateRef.set(iDateTotal, function(error) {
+            iDateRef.once("value", function(snapshotB) {
+              var data = snapshotB.val();
+            });
+          });
+        }
+      }
+    }
+  });
+
+}
+
+// Calculate Data Totals for a specific day object raw data node
+function calculateFirebaseObjDayTotal(obj) {
+  var dayTotal = 0;
+  var deltaLimit = 5;
+  for (var key in obj) {
+    var differential = obj[key].differential;
+    if (Math.abs(differential) <= deltaLimit) {
+      // console.log(differential);
+      // ignore any changes more than 5 cups
+      dayTotal -= differential;
+    }
+  }
+  return dayTotal;
+}
+
+// Calculate Totals up until today
+function calculateTotals() {
+  var total = 0;
+  var startDate = new Date("2015-08-05");
+  var currentDate = new Date();
+  var timeDiff = Math.abs(currentDate.getTime() - startDate.getTime());
+  var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)) - 1; 
+
+  var currentDateYmd = moment().format("YYYY-MM-DD");
+
+
+  var totalsRef = new Firebase('https://papercup22.firebaseio.com/paper_cup_totals');
+  totalsRef.on('value', function(snapshot) {
+    var totalsObj = snapshot.val();
+
+    snapshot.forEach(function(data) {
+      total += data.val();
+    });
+
+
+    return total;
+  });
+}
+
+function calculateTotalsData(obj) {
+  var total = 0;
+
+  for (var key in obj) {
+    total += obj[key];
+  }
+
+  return total;
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(window).load(function(){
+  var currentDate = moment().format("YYYY-MM-DD");
+  initDbTotals();
+  var previousTotals = 0;
+  // console.log(previousTotals);
 
   var totalNum = '000000';
   var yesterdayNum = '0000';
@@ -25,41 +29,79 @@ $(window).load(function(){
   // yesterdayDisplay.splitflap("value", yesterdayNum);
   // todayDisplay.splitflap("value", pad(todayNum,4));
 
+  // Listen to Firebase
+  var myDataRef = new Firebase('https://papercup22.firebaseio.com/');
+  myDataRef.once('value', function(snapshot) {
+    var todayDate = new Date();
+    var yesterdayDate = new Date();
 
-  // Pad some numbers
-  function pad(n, width, z) {
-    z = z || '0';
-    n = n + '';
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-  }
+    yesterdayDate.setDate(yesterdayDate.getDate() - 1);
+
+    var todayDateYmd = moment(todayDate).format("YYYY-MM-DD");
+    var yesterdayDateYmd = moment(yesterdayDate).format("YYYY-MM-DD");
+
+    var obj = snapshot.val();
+
+    yesterdayNum = obj.paper_cup_totals[yesterdayDateYmd]; // Yesterday
+    totalNum = calculateTotalsData(obj.paper_cup_totals); // Total from previous days
+    todayNum = calculateFirebaseObjDayTotal(obj.paper_cup_count[todayDateYmd]); // Today
+
+    refreshValues(); // Refresh
+  });
+
+  // Trigger changes
+  myDataRef.on('value', function(snapshot) {
+    var todayDate = new Date();
+    var yesterdayDate = new Date();
+
+    yesterdayDate.setDate(yesterdayDate.getDate() - 1);
+
+    var todayDateYmd = moment(todayDate).format("YYYY-MM-DD");
+    var yesterdayDateYmd = moment(yesterdayDate).format("YYYY-MM-DD");
+
+    var obj = snapshot.val();
+
+    yesterdayNum = obj.paper_cup_totals[yesterdayDateYmd]; // Yesterday
+    totalNum = calculateTotalsData(obj.paper_cup_totals); // Total from previous days
+    todayNum = calculateFirebaseObjDayTotal(obj.paper_cup_count[todayDateYmd]); // Today
+
+    refreshValues(); // Refresh
+  });
+
+
 
   // Listen to Total
-  var myDataRefTotal = new Firebase('https://boiling-torch-7735.firebaseio.com/cups/total');
-  myDataRefTotal.on('value', function(snapshot) {
-    var totalValue = snapshot.val();
-    totalNum = totalValue;
-    // totalDisplay.splitflap("value", pad(totalNum+todayNum,6));
-  });
+  // var myDataRefTotal = new Firebase('https://boiling-torch-7735.firebaseio.com/cups/total');
+  // myDataRefTotal.on('value', function(snapshot) {
+  //   var totalValue = snapshot.val();
+  //   totalNum = totalValue;
+  // });
 
-  // Listen to Yesterday
-  var myDataRefYesterday = new Firebase('https://boiling-torch-7735.firebaseio.com/cups/yesterday');
-  myDataRefYesterday.on('value', function(snapshot) {
-    var yesterdayValue = snapshot.val();
-    yesterdayNum = yesterdayValue;
+  // // Listen to Yesterday
+  // var myDataRefYesterday = new Firebase('https://boiling-torch-7735.firebaseio.com/cups/yesterday');
+  // myDataRefYesterday.on('value', function(snapshot) {
+  //   var yesterdayValue = snapshot.val();
+  //   yesterdayNum = yesterdayValue;
+  //   yesterdayDisplay.splitflap("value", pad(yesterdayNum,4));
+  //   updateCompare();
+  // });
+
+  // // Listen to Today
+  // var myDataRefToday = new Firebase('https://boiling-torch-7735.firebaseio.com/cups/today');
+  // myDataRefToday.on('value', function(snapshot) {
+  //   var todayValue = snapshot.val();
+  //   todayNum = todayValue;
+  //   todayDisplay.splitflap("value", pad(todayNum,4));
+  //   totalDisplay.splitflap("value", pad(todayNum+totalNum,6));
+  //   updateCompare();
+  // });
+
+  function refreshValues() {
     yesterdayDisplay.splitflap("value", pad(yesterdayNum,4));
-    updateCompare();
-  });
-
-  // Listen to Today
-  var myDataRefToday = new Firebase('https://boiling-torch-7735.firebaseio.com/cups/today');
-  myDataRefToday.on('value', function(snapshot) {
-    var todayValue = snapshot.val();
-    todayNum = todayValue;
-    todayDisplay.splitflap("value", pad(todayNum,4));
+    todayDisplay.splitflap("value", pad(todayNum,4))
     totalDisplay.splitflap("value", pad(todayNum+totalNum,6));
     updateCompare();
-  });
-
+  }
   function updateCompare() {
     if (yesterdayNum<=todayNum) {
       $("#compare").html("<");
@@ -67,5 +109,13 @@ $(window).load(function(){
     else {
       $("#compare").html(">");
     }
+  }
+
+
+  // Pad some numbers
+  function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
   }
 });
